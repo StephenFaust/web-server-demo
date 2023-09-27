4b8eb7c85c04d401531610c23055bc322e28b5f4
huangjinmao <1094074803@qq.com>
2023-09-27 10:48:13 +0800

    init

--- web-server-main/src/main.rs
+++ web-server-main/src/main.rs
@@ -1,46 +1,37 @@
-use std::{thread, time::Duration};
+use std::{
+    fs,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 use thread_pool::ThreadPool;
 
-
-
 fn main() {
     let mut thread_pool = ThreadPool::new(10);
-    for i in 1..100 {
-        // thread::sleep(Duration::from_secs(1));
-        thread_pool
-            .execute(move || {
-                println!("current thread {:?} ,num: {}", thread::current().id(), i);
-                thread::sleep(Duration::from_secs(10));
-            })
-            .unwrap();
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread_pool.execute(|| handle_connection(stream)).unwrap();
     }
 
-    loop {}
-
-    // let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
-    //     thread::spawn(|| handle_connection(stream));
-    // }
-
-    // fn handle_connection(mut stream: TcpStream) {
-    //     let mut buffer = [0; 1024];
-    //     stream.read(&mut buffer).unwrap();
-    //     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    //     let contents = fs::read_to_string("hello.html").unwrap_or_else(|err| {
-    //         println!("{}", err);
-    //         String::from("value")
-    //     });
-    //     let thread_id =
-    //     println!("current threadId is {:?}", thread_id);
-    //     let response = format!(
-    //         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-    //         contents.len(),
-    //         contents
-    //     );
-    //     stream.write(response.as_bytes()).unwrap();
-    //     stream.flush().unwrap();
-    // }
+    fn handle_connection(mut stream: TcpStream) {
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        let contents = fs::read_to_string("hello.html").unwrap_or_else(|err| {
+            println!("{}", err);
+            String::from("value")
+        });
+        println!("current threadId is {:?}", thread::current().id());
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
